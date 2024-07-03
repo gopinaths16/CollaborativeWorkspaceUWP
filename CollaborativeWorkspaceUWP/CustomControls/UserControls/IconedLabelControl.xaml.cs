e08ad2040de1d1ac837b239be3a5aafd7b61ab79
Gopinath S <gopinaths16@protonmail.com>
2024-07-03 17:32:59 +0530

    feat: Daily commit 2 - Base Task details view defined - 03/07/2024

---
+++ CollaborativeWorkspaceUWP/CustomControls/UserControls/IconedLabelControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CollaborativeWorkspaceUWP.CustomControls.UserControls
+{
+    public sealed partial class IconedLabelControl : UserControl
+    {
+
+        public string Icon
+        {
+            get { return (string)GetValue(LabelIconProperty); }
+            set { SetValue(LabelIconProperty, value); }
+        }
+
+        public string ContentText
+        {
+            get { return (string)GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelIconProperty = DependencyProperty.Register("Icon", typeof(string), typeof(IconedLabelControl), null);
+
+        public static readonly DependencyProperty ContentProperty = DependencyProperty.Register("ContentText", typeof(string), typeof(IconedLabelControl), null);
+
+
+        public IconedLabelControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
