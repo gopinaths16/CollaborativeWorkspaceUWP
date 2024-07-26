8d4c6effd158ae9f443451736790963c60b537fc
Gopinath S <gopinaths16@protonmail.com>
2024-07-26 18:06:37 +0530

    Daily Commit 2 - 26/07/2024

--- CollaborativeWorkspaceUWP/CustomControls/UserControls/AttachmentItemControl.xaml.cs
+++ CollaborativeWorkspaceUWP/CustomControls/UserControls/AttachmentItemControl.xaml.cs
@@ -1,17 +1,22 @@
 ﻿using CollaborativeWorkspaceUWP.Models;
+using CollaborativeWorkspaceUWP.Utilities;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
@@ -20,6 +25,8 @@ namespace CollaborativeWorkspaceUWP.CustomControls.UserControls
 {
     public sealed partial class AttachmentItemControl : UserControl
     {
+        private Attachment attachment;
+
         public AttachmentItemControl()
         {
             this.InitializeComponent();
@@ -33,7 +40,43 @@ namespace CollaborativeWorkspaceUWP.CustomControls.UserControls
         private void Grid_PointerExited(object sender, PointerRoutedEventArgs e)
         {
             VisualStateManager.GoToState(this, "Normal", true);
+        }
 
+        private async void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            attachment = this.DataContext as Attachment;
+            await SetPreviewItem();
+        }
+
+        private async Task SetPreviewItem()
+        {
+            if(attachment != null)
+            {
+                switch(attachment.Type)
+                {
+                    case "text/plain":
+                        IconElement.Visibility = Visibility.Visible;
+                        IconElement.Glyph = "\ue922";
+                        IconElement.Foreground = Util.CreateBrushFromHex("#4593fb");
+                        break;
+
+                    case "application/pdf":
+                        IconElement.Visibility = Visibility.Visible;
+                        IconElement.Glyph = "\ueadf";
+                        IconElement.Foreground = Util.CreateBrushFromHex("#f70000");
+                        break;
+
+                    case "image/png":
+                    case "image/jpeg":
+                    case "image/jpg":
+                        PreviewImage.Visibility = Visibility.Visible;
+                        BitmapImage bitmapImage = new BitmapImage();
+                        bitmapImage.UriSource = new Uri(ApplicationData.Current.LocalFolder.Path + Path.DirectorySeparatorChar + "Attachments" + Path.DirectorySeparatorChar + attachment.Path);
+                        PreviewImage.Source = bitmapImage;
+                        break;
+
+                }
+            }
         }
     }
 }
---
+++ CollaborativeWorkspaceUWP/Utilities/Util.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace CollaborativeWorkspaceUWP.Utilities
+{
+    public class Util
+    {
+
+        public static SolidColorBrush CreateBrushFromHex(string hex)
+        {
+            if (hex.StartsWith("#"))
+            {
+                hex = hex.Substring(1);
+            }
+
+            byte a = 255;
+
+            if (hex.Length == 8)
+            {
+                a = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+                hex = hex.Substring(2);
+            }
+
+            byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+            byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+            byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+
+            Color color = Color.FromArgb(a, r, g, b);
+            return new SolidColorBrush(color);
+        }
+
+    }
+}
